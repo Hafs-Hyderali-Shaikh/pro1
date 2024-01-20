be0fda6f43bbfeee99aa073602cddf205adb887d
Hafs Hyderali Shaikh <hafshyderali@gmail.com>
2024-01-20 10:50:23 +0400

    initial commit

---
+++ variables.py
@@ -0,0 +1,12 @@
+def variables():
+    ageMonths = "225 months"
+    daysYear = "365 days"
+    firstPet ="Haroon"
+    first_5_digits_of_Pi = "3.14159"
+    print("ageMonths =",ageMonths)
+    print("daysYear =",daysYear)
+    print("firstPet =",firstPet)
+    print("first_5_digits_of_Pi =",first_5_digits_of_Pi)
+    
+variables()
+    
