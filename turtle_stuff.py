b4bff5d684a468fe3179a7cc914d51812f2112db
Hafs Hyderali Shaikh <hafshyderali@gmail.com>
2024-01-20 11:55:02 +0400

    change

--- turtle_stuff.py
+++ turtle_stuff.py
@@ -12,8 +12,19 @@ def test_drive():
     t.home()
     t.circle(25)
     
+    
+def turtle_state():
+    print("Pen is down:", turtle.isdown())
+    print("Heading angle:", turtle.heading())
+    print("Current coordinates (X, Y):", turtle.xcor(), turtle.ycor())
+    
+    
+    
 def Main():
+    turtle_state()
     test_drive()
+    print("Turtle state after test_drive:")
+    turtle_state()
     input("please enter to continue...")
     
 Main()
