467b81994ce69f001395d87bfda06ac1ca88d0c0
Hafs Hyderali Shaikh <hafshyderali@gmail.com>
2024-01-20 11:11:04 +0400

    changes commit

---
+++ happy_birthday.py
@@ -0,0 +1,16 @@
+
+def birthday():
+    Name = input("Give a name:")
+    BirthMonth = input("Birth month:")
+    BirthDay = input("Birthday:")
+    Birth_Year = input("Birth Year:")
+    print(Name,"your birthday is on",BirthMonth, BirthDay,",", Birth_Year,"!")
+    
+birthday()
+#assssssssssss
+'''
+aaaaaakdckckccs
+'''
+'''
+jnn
+'''
