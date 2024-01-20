f796a0c055016ccd63168a02ef92da570a77f702
Hafs Hyderali Shaikh <hafshyderali@gmail.com>
2024-01-20 11:11:31 +0400

    changes commit

--- variables.py
+++ variables.py
@@ -9,4 +9,17 @@ def variables():
     print("first_5_digits_of_Pi =",first_5_digits_of_Pi)
     
 variables()
-    
+
+def expressions_practice():
+    Literal = 10
+    Addition = 5+19
+    Exponent = 2**10
+    Mod = 5%56
+    multiplication = (59+2-19)*5
+    mix_operators = 5+9-20*5/9
+    print(Literal)
+    print(Addition)
+    print(Exponent)
+    print(Mod)
+    print(multiplication)
+    print(mix_operators)
